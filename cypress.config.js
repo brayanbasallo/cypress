c47e553ece62f50e5762c23553aa740f8f54050d
Brayan Basallo Soto <basallo17@gmail.com>
2022-09-27 22:03:01 -0500

    First test and initial config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  viewportWidth:1920,
+  viewportHeight:1080,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    excludeSpecPattern: [
+       "cypress/e2e/1-getting-started",
+       "cypress/e2e/2-advanced-examples"
+    ]
   },
 });
---
+++ cypress/e2e/tests/firstTest.cy.js
@@ -0,0 +1,5 @@
+describe('first test', () => {
+	it('go to first page', () => {
+		cy.visit('https://platzi.com/')
+	})
+})
